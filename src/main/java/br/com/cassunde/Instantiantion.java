ed35186e7687a7222c119f3508c64bf5d34c5d4e
Mattheus Cassundé <mattheus.089@gmail.com>
2018-08-16 22:07:42 -0300

    feat(Post): added new Document, the 'POST'
    - added document join 'Post' and 'User'

--- src/main/java/br/com/cassunde/Instantiantion.java
+++ src/main/java/br/com/cassunde/Instantiantion.java
@@ -1,12 +1,15 @@
 package br.com.cassunde;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.cassunde.domain.Post;
 import br.com.cassunde.domain.User;
+import br.com.cassunde.repository.PostRepository;
 import br.com.cassunde.repository.UserRepository;
 
 /**
@@ -19,6 +22,9 @@ public class Instantiantion implements CommandLineRunner {
 	@Autowired
 	private UserRepository rep;
 	
+	@Autowired
+	private PostRepository repPost;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -28,7 +34,15 @@ public class Instantiantion implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-	
 		rep.saveAll( Arrays.asList( maria, alex, bob) );
+		
+		repPost.deleteAll();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Post post1 = new Post(null, sdf.parse("01/01/2018"), "Prmeiro post", "Descrição do Prmeiro post", maria);
+		Post post2 = new Post(null, sdf.parse("01/01/2018"), "Segundo post", "Descrição do Prmeiro 2", maria);
+	
+		repPost.saveAll(Arrays.asList(post1, post2));
 	}
 }
